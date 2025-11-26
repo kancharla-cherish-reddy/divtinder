ed2df10c573e391161d1abad534ef5b8f04c9e9d
cherish reddy <cherishreddy2004@gmail.com>
2025-11-26 13:23:19 +0530

    my 2nd commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user/:username/:dateofbirth", (req, res) => {
-  res.send("hello");
+app.use("/user", (req, res, next) => {
+  const token = 123;
+  const verifytoken = token === 123;
+  if (verifytoken) {
+    next();
+  } else {
+    console.log("error");
+    return res.status(401).send("something went wronng");
+  }
+});
 
-  console.log(req.params);
+app.get("/user/getalldata", (req, res) => {
+  res.send("sent all the data");
+});
+
+app.get(
+  "/home",
+  (req, res, next) => {
+    console.log("first handler");
+    next();
+  },
+  (req, res, next) => {
+    console.log("second handler");
+    next();
+  },
+  (req, res, next) => {
+    res.send("final router handler");
+    // next();
+  }
+);
+
+app.get("/login", (req, res, next) => {
+  try {
+    throw new Error("hello from error");
+    console.log("hello");
+    res.send("hello from try");
+  } catch (err) {
+    console.log("something is wrong");
+    res.status(401).send("hello from catch");
+  }
 });
 
 app.listen(2000, () => {
